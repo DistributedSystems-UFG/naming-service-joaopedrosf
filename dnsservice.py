ed11a83af5b2a974346330ecd6f83865575a016f
joaopedrosf <joaopedrosf1903@gmail.com>
2023-12-09 11:28:48 -0300

    DNS Service

---
+++ dnsservice.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+from const import *
+
+app = Flask(__name__)
+service_registry = {}
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    service_name = data.get('serviceName')
+    url = data.get('url')
+
+    if service_name and url:
+        service_registry[service_name] = url
+        return jsonify(), 201
+    else:
+        return jsonify({'error': 'Invalid request payload'}), 400
+
+@app.route('/lookup', methods=['GET'])
+def lookup():
+    service_name = request.args.get('serviceName')
+
+    if service_name in service_registry:
+        return jsonify({'url': service_registry[service_name]})
+    else:
+        return jsonify({'error': f'Service {service_name} not found'}), 404
+
+@app.route('/unregister', methods=['DELETE'])
+def unregister():
+    service_name = request.args.get('serviceName')
+
+    if service_name in service_registry:
+        del service_registry[service_name]
+        return jsonify(), 204
+    else:
+        return jsonify({'error': f'Service {service_name} not found'}), 404
+
+if __name__ == '__main__':
+    app.run(port=PORT)
